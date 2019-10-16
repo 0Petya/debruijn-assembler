4d039a85967adc63cd9c3144433804880c3e62c2
Peter Tran <Peter1992t@gmail.com>
2019-10-16 10:53:38 -0500

    Form overlaps

--- src/elm/DeBruijn.elm
+++ src/elm/DeBruijn.elm
@@ -22,11 +22,31 @@ generateKMers sequences k =
     concatMap slidingSlice sequences
 
 
+identifyOverlaps : String -> List String -> List String
+identifyOverlaps kmer kmers =
+    let
+        overlapSubject : String
+        overlapSubject =
+            String.dropLeft 1 kmer
+    in
+    filter (\target -> String.dropRight 1 target == overlapSubject) kmers
+
+
 compileDot : List String -> Int -> String
 compileDot sequences k =
     let
         kmers : List String
         kmers =
             generateKMers sequences k
+
+        overlapGroups : List ( String, List String )
+        overlapGroups =
+            map (\kmer -> ( kmer, identifyOverlaps kmer kmers )) kmers
+
+        digraphConnections : ( String, List String ) -> String
+        digraphConnections ( kmer, overlaps ) =
+            overlaps
+                |> map (\overlap -> kmer ++ " -> " ++ overlap)
+                |> String.join "\n"
     in
-    "digraph {" ++ String.join "\n" kmers ++ "}"
+    "digraph {" ++ (String.join "\n" <| map digraphConnections overlapGroups) ++ "}"

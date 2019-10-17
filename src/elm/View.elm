6b941dbab7a9dc692c9fb4de505ae287048b18ca
Peter Tran <Peter1992t@gmail.com>
2019-10-17 14:45:30 -0500

    Add styling

--- src/elm/View.elm
+++ src/elm/View.elm
@@ -10,11 +10,19 @@ import Model exposing (Model)
 view : Model -> Html Msg
 view model =
     div [ id "elm" ]
-        [ textarea [ class "sequence-input", onInput SequenceInput ]
-            []
-        , input [ class "k-input", onInput KInput ] []
-        , button
-            [ onClick Generate ]
+        [ h2 [ class "header" ]
+            [ text "De Bruijn Graph Generator" ]
+        , a [ class "wikipedia-link", href "https://en.wikipedia.org/wiki/De_Bruijn_graph" ]
+            [ text "Wikipedia" ]
+        , label []
+            [ text "Sequences"
+            , textarea [ class "sequence-input", onInput SequenceInput ] []
+            ]
+        , label []
+            [ text "k"
+            , input [ class "k-input", onInput KInput ] []
+            ]
+        , button [ class "generate", onClick Generate ]
             [ text "Generate" ]
         , div [ class "error-messages" ] <| List.map (\error -> p [] [ text error ]) model.errors
         ]
